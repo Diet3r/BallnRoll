6ff6710b6916761bef3230fc49fb2ed1de071477
Diet3r <86099270+Diet3r@users.noreply.github.com>
2023-04-19 02:03:11 +0200

    PlayerMovement rework
    +PlayerMovement got a rework
    +new curve prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-    GameObject camera;
+    GameObject Maincamera;
 
     float rollSpeed = 70f;
     public float maxRollSpeed = 9f;
 
     float xAxis;
     float zAxis;
+    Vector3 oldPosition = new Vector3();
     Vector3 cameraAxis;
 
 
@@ -19,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        Maincamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
@@ -32,17 +33,81 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         MovementForce();
-        Geschwindigkeitsbegrenzer();
+        //Geschwindigkeitsbegrenzer();
     }
 
     void MovementForce()
     {
         xAxis = Input.GetAxis("Horizontal");
-        cameraAxis = camera.transform.TransformDirection(1, 0, 1);
+        //cameraAxis = Maincamera.transform.TransformDirection(1, 0, 1);
         zAxis = Input.GetAxis("Vertical");
+        Vector3 newPosition = rb.position;
+        Debug.Log("x: " + Mathf.Abs(newPosition.x - oldPosition.x) + "   z: " + Mathf.Abs(newPosition.z - oldPosition.z));
 
 
-        rb.AddForce(xAxis * cameraAxis.x * rollSpeed, 0, zAxis * cameraAxis.z * rollSpeed);
+        if (rb.velocity.x >= 0)
+        {
+            if (xAxis > 0)
+            {
+                if (Mathf.Abs(newPosition.x - oldPosition.x) <= maxRollSpeed * Time.deltaTime)
+                {
+                    rb.AddForce(xAxis * rollSpeed, 0, 0);
+                }
+            }
+            else if (xAxis < 0)
+            {
+                rb.AddForce(xAxis * rollSpeed, 0, 0);
+            }
+        }
+        if (rb.velocity.x <= 0)
+        {
+            if (xAxis < 0)
+            {
+                if (Mathf.Abs(newPosition.x - oldPosition.x) <= maxRollSpeed * Time.deltaTime)
+                {
+                    rb.AddForce(xAxis * rollSpeed, 0, 0);
+                }
+            }
+            else if (xAxis > 0)
+            {
+                rb.AddForce(xAxis * rollSpeed, 0, 0);
+            }
+        }
+
+        if (rb.velocity.z >= 0)
+        {
+            if (zAxis > 0)
+            {
+                if (Mathf.Abs(newPosition.z - oldPosition.z) <= maxRollSpeed * Time.deltaTime)
+                {
+                    rb.AddForce(0, 0, zAxis * rollSpeed);
+                }
+            }
+            else if (zAxis < 0)
+            {
+                rb.AddForce(0, 0, zAxis * rollSpeed);
+            }
+        }
+        else if (rb.velocity.z <= 0)
+        {
+            if (zAxis < 0)
+            {
+                if (Mathf.Abs(newPosition.z - oldPosition.z) <= maxRollSpeed * Time.deltaTime)
+                {
+                    rb.AddForce(0, 0, zAxis * rollSpeed);
+                }
+            }
+            else if (zAxis > 0)
+            {
+                rb.AddForce(0, 0, zAxis * rollSpeed);
+            }
+        }
+
+        oldPosition = newPosition;
+
+        //rb.AddForce(xAxis * cameraAxis.x * rollSpeed, 0, zAxis * cameraAxis.z * rollSpeed);
+        //rb.AddForce(xAxis * rollSpeed, 0, zAxis * rollSpeed);
+        //mathf.abs
     }
 
     void Geschwindigkeitsbegrenzer()
